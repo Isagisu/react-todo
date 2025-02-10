eda7c15d18550333320a2d0be98acea80a8c0783
isagisu <a0084428@yahoo.com>
2025-02-11 00:02:05 +0800

    初始化專案

---
+++ my-react-todo/src/components/Todo.jsx
@@ -0,0 +1,6 @@
+function Todo() {
+  return <div className="todo">
+    <p>洗衣服</p>
+  </div>  
+}
+export default Todo
---
+++ my-react-todo/src/components/TodoWrapper.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import CreateForm from "./CreateForm";
+import Todo from "./Todo";
+
+function TodoWrapper() {
+  const [todos,setTodos]=useState(['打掃廁所','寫作業']);
+  return (
+    <div className="wrapper">
+      <h1>待辦事項</h1>
+      <CreateForm />
+      <Todo />
+    </div>
+  );
+}
+
+export default TodoWrapper;
